45c359fe0e39d0c369a179ae8b3cab097ceb497d
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-06-25 22:02:31 +0330

    refactor(main): slide function

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import pandas as pd
 class Alphabet:
     def __init__(self) -> None:
         self.dic_alpha: Dict[str, str] = {"ب": "ب", "پ": "پ", "ت": "ت", "م": "م"}
-    
+
     def get_alpha_key(self, entry: "WordEntry") -> str:
         return self.dic_alpha[entry.get_key()]
 
 
 alphabet = Alphabet()
+
 
 class WordEntry:
     def __init__(self, word: str, translation: str):
@@ -22,26 +23,29 @@ class WordEntry:
 
     def __get_context(self):
         return [self.__translation, self.__word]
-    
+
     def __eq__(self, value: object) -> bool:
         if isinstance(value, list):
             return value == self.__get_context()
         return False
 
 
+def put_entry_in_context(context: Dict[str, list], entry: "WordEntry"):
+    if entry.get_key() in alphabet.dic_alpha.keys():
+        alpha_key = alphabet.get_alpha_key(entry)
+
+        if not context.__contains__(alpha_key):
+            context[alpha_key] = []
+        context[alpha_key].append(entry)
+
+
 def df_to_context(df: "pd.DataFrame") -> "Dict[str, list]":
     context: Dict[str, list] = dict()
 
-    def put_entry_in_context(entry: "WordEntry"):
-        if entry.get_key() in alphabet.dic_alpha.keys():
-            alpha_key = alphabet.get_alpha_key(entry)
-
-            if not context.__contains__(alpha_key):
-                context[alpha_key] = []
-            context[alpha_key].append(entry)
-
     for j in df.itertuples():
-        put_entry_in_context(entry=WordEntry(word=j.word, translation=j.translation))
+        put_entry_in_context(
+            context=context, entry=WordEntry(word=j.word, translation=j.translation)
+        )
 
     return context
 
