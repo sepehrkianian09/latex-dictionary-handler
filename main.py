a4e2b29c14271a89aa03ec0a42e699bd034d0bb4
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-06-25 21:47:15 +0330

    refactor(main): transform functions into class

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ dic_alpha: Dict[str, str] = {"ب": "ب", "پ": "پ", "ت": "ت", "م": "م"}
 
 alpha: list[str] = list(dic_alpha.values())
 
+
+class WordEntry:
+    def __init__(self, word: str, translation: str):
+        self.__word = word
+        self.__translation = translation
+
+    def get_key(self):
+        return self.__translation[0]
+
+    def get_context(self):
+        return [self.__translation, self.__word]
+
+
 def df_to_context(df: "pd.DataFrame") -> "Dict[str, list]":
     context: Dict[str, list] = dict()
     for i in alpha:
         context[i] = []
 
     for j in df.itertuples():
-        eng: str = j.word
-        fa: str = j.translation
-        
-        def get_key():
-            return fa[0]
-        
-        def get_jj():
-            return [fa, eng]
+        word_entry = WordEntry(word=j.word, translation=j.translation)
 
-        if get_key() in dic_alpha.keys():
-            alpha_key = dic_alpha[get_key()]
+        if word_entry.get_key() in dic_alpha.keys():
+            alpha_key = dic_alpha[word_entry.get_key()]
 
-            context[alpha_key].append(get_jj())
+            context[alpha_key].append(word_entry.get_context())
 
     return context
 
